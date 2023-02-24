14e186e80580b1587dd12f4f0ce6fa3f6ee53016
OrestShylan <orestshylan1997@gmail.com>
2023-02-24 19:28:25 +0200

    update function, loadmore btn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import './sass/main.scss';
+import './sass/index.scss';
 import { fetchImages } from './js/fetch-images';
 import { renderGallery } from './js/render-gallery';
-import simpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
@@ -15,3 +16,65 @@ const perPage = 40;
 
 searchForm.addEventListener('submit', onSearchForm);
 loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+function onSearchForm(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0 });
+  page = 1;
+  query = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  loadMoreBtn.classList.add('is-hidden');
+
+  if (query === ' ') {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+        if (data.totalHits > perPage) {
+          loadMoreBtn.classList.remove('is-hidden');
+        }
+      }
+    })
+    .catch(onError)
+    .finally(() => searchForm.reset());
+}
+function onError(err) {
+  Notify.failure('OOOOPS ! IT IS FIACSO,BRO!');
+  console.log(error);
+}
+
+function onLoadMoreBtn() {
+  page += 1;
+  simpleLightBox.destroy();
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page > totalPages) {
+        loadMoreBtn.classList.add('is-hidden');
+        Notify.failure(
+          `We're sorry, but you've reached the end of search results.`
+        );
+      }
+    })
+    .catch(error => console.log(error));
+  const totalPages = Math.ceil(data.totalHits / perPage);
+  if (page > totalPages) {
+  }
+}
